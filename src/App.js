33377ef5317f1b3d4d5946fd8c3d822e9a2e1d8e
Margotte83 <margotreize@live.fr>
2021-10-21 13:53:29 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import WalletCardEthers from'./WalletCardEthers';
+
+function App() {
+
+  return (
+    <div className="App">
+    <WalletCardEthers/>
+    </div>
+  );
+}
+
+export default App;
